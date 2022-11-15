ca7f10d2af0a4189351e6bc04901bd55678fec26
krus0705 <jacob.crotherz@gmail.com>
2022-11-15 09:54:45 -0500

    frontend layout

---
+++ src/components/Layout/Content.element.js
@@ -0,0 +1,9 @@
+import styled, {css} from "styled-components";
+
+export const ContentStyle = styled.div`
+    cursor: pointer;
+    background-color: orange;
+    &:hover {
+        transform: translate(0.2rem, -0.5ex);
+    }
+`;
